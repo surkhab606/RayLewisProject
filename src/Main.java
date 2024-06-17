7358c25928e0fe21cc2f7ec758ae01e877d2f6dd
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-06-16 20:13:09 -0600

    Implemented random class to generate random numbers for program. Established main program loop and card generation. Updated scanner usage.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,9 @@ public class Main {
         //Set up user input scanner
         Scanner scanner = new Scanner(System.in);
 
+        //Set up use of random class
+        Random rand = new Random();
+
         //Prompt user to pick a card
         System.out.println("Type p to pick a card.");
 
@@ -18,7 +22,34 @@ public class Main {
         //If the user's input equals lower or upper case P, then move into main program loop...
         if (input.equals("p") || input.equals("P")) {
 
+            //Obtain a number between 0 and 12
+            int bound = rand.nextInt(13);
 
+            //Add a 1 to the bound so that the bound is between 1 and 13.
+            bound += 1;
+
+            //If you pull an ace, do 35 push-ups.
+            if (bound == 1) {
+                System.out.println("You just got an Ace. Do 35 push-ups.");
+            }
+
+            //If you pull an ace, do 55 push-ups.
+            else if (bound == 11) {
+                System.out.println("You just got a Jack. Do 55 push-ups.");
+            }
+
+            //If you pull an ace, do 20 push-ups.
+            else if (bound == 12) {
+                System.out.println("You just got a Queen. Do 20 push-ups.");
+            }
+
+            //If you pull an ace, do 20 push-ups.
+            else if (bound == 13) {
+                System.out.println("You just got a King. Do 20 push-ups.");
+            }
+
+            else if (bound == 2){
+            }
         }
 
         //Else, end it right there.
