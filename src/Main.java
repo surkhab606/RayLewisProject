5e9b39f34aab9cf6f14e6c8db38b9006219018fe
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-06-16 20:38:51 -0600

    Added replay functionality naturally - players must choose to quit. Closed scanner to avoid resource leak.

--- src/Main.java
+++ src/Main.java
@@ -13,56 +13,68 @@ public class Main {
         //Set up use of random class
         Random rand = new Random();
 
+        //Set up boolean to determine if player wants to continue picking cards
+        boolean continuePicking = true;
+
         //Prompt user to pick a card
-        System.out.println("Type p to pick a card.");
+        System.out.println("Type p to pick a card. Type q to quit.");
 
         //Store user's input
         String input = scanner.nextLine();
 
-        //If the user's input equals lower or upper case P, then move into main program loop...
-        if (input.equals("p") || input.equals("P")) {
+        while(continuePicking) {
+            //If the user's input equals lower or upper case P, then move into main program loop...
+            if (input.equals("p") || input.equals("P")) {
 
-            //Obtain a number between 0 and 12
-            int bound = rand.nextInt(13);
+                //Obtain a number between 0 and 12
+                int bound = rand.nextInt(13);
 
-            //Add a 1 to the bound so that the bound is between 1 and 13.
-            bound += 1;
+                //Add a 1 to the bound so that the bound is between 1 and 13.
+                bound += 1;
 
-            switch(bound) {
+                switch (bound) {
 
-                //If the random number generator generates a 1, you pulled an ace.
-                case 1:
-                    System.out.println("You pulled an Ace. Do 35 push-ups.");
-                    break;
+                    //If the random number generator generates a 1, you pulled an ace.
+                    case 1:
+                        System.out.println("You pulled an Ace. Do 35 push-ups.");
+                        break;
 
-                //If the random number generator generates an 11, you pulled a jack.
-                case 11:
-                    System.out.println("You pulled a Jack. Do 55 push-ups.");
-                    break;
+                    //If the random number generator generates an 11, you pulled a jack.
+                    case 11:
+                        System.out.println("You pulled a Jack. Do 55 push-ups.");
+                        break;
 
-                //If the random number generator generates a 12, you pulled a queen.
-                case 12:
-                    System.out.println("You pulled a Queen. Do 20 push-ups.");
-                    break;
+                    //If the random number generator generates a 12, you pulled a queen.
+                    case 12:
+                        System.out.println("You pulled a Queen. Do 20 push-ups.");
+                        break;
 
-                //If the random number generator generates a 13, you pulled a king.
-                case 13:
-                    System.out.println("You pulled a king. Do 20 push-ups.");
-                    break;
+                    //If the random number generator generates a 13, you pulled a king.
+                    case 13:
+                        System.out.println("You pulled a king. Do 20 push-ups.");
+                        break;
 
-                //If the random number generator generates anything from 2-10, you pulled a 2-10 numbered card.
-                default:
-                System.out.println("You pulled a " + bound + ". Do " + bound + " push-ups.");
-                break;
+                    //If the random number generator generates anything from 2-10, you pulled a 2-10 numbered card.
+                    default:
+                        System.out.println("You pulled a " + bound + ". Do " + bound + " push-ups.");
+                        break;
+                }
+
+            }
+            //If the player picks q, quit
+            else if (input.equals("q") || input.equals("Q")) {
+                continuePicking = false;
+                System.out.println("Hope you aren't quitting without doing any push-ups.");
             }
 
+            //Else, end it right there.
+            else {
+                System.out.println("Maybe do some push-ups next time, fatty.");
             }
-
-        //Else, end it right there.
-        else {
-
-            System.out.println("Maybe do some push-ups next time, fatty.");
         }
+
+        //Scanner close
+        scanner.close();
 
 
 
